715fc5c4de45ed9c9ed3b934f46437390cac19d4
230049 <230049@edu.p.lodz.pl>
2021-10-23 17:15:47 +0200

    FIXED THAT SHIT

--- chase/Wolf.py
+++ chase/Wolf.py
@@ -3,36 +3,37 @@ import math
 
 
 class Wolf:
-    def __init__(self, x, y, wolf_move_dist, sheep):
+    def __init__(self, x, y, wolf_move_dist):
         self.victim = None
         self.sheep_number = 0
         self.x = x
         self.y = y
         self.wolf_move_dist = wolf_move_dist
         self.is_chasing = False
-        self.sheep = sheep
 
-    def move_wolf(self):
+    def move_wolf(self, sheep):
 
         if self.is_chasing is False:
             self.is_chasing = True
             sheep_distances = []
 
-            for shp in self.sheep:
+            for shp in sheep:
                 if shp.is_dead is False:
                     sheep_distances.append(self.check_distance(shp.x, shp.y))
 
-            sheep_number = self.sheep[sheep_distances.index(min(sheep_distances))].id_sheep
+            sheep_number = sheep[sheep_distances.index(min(sheep_distances))].id_sheep
             self.sheep_number = sheep_number
-            self.victim = next((s for s in self.sheep if s.id_sheep == self.sheep_number), None)
+            self.victim = next((s for s in sheep if s.id_sheep == self.sheep_number), None)
 
-            self.chase()
+            self.chase(sheep)
             logging.debug("Function chase(" + str(self) + ") was called")
+            print('if')
 
         else:
-            self.chase()
+            self.chase(sheep)
+            print('else')
 
-    def chase(self):
+    def chase(self, sheep):
         distance_wolf = self.check_distance(self.victim.x, self.victim.y)
         logging.debug("Function check_distance(" + str(self.victim.x) + " " + str(self.victim.y)
                       + ") was called on a Wolf object: " + str(self)
@@ -46,6 +47,7 @@ class Wolf:
             self.y = self.victim.y
             self.victim.is_dead = True
             self.is_chasing = False
+            print('jol')
 
         else:
             move_to_x = self.wolf_move_dist \
